b3fea0c913bab915c0f7457cf22ca1ab90b88282
Daan Debie <d.debie@thenewmotion.com>
2015-02-17 21:41:28 +0100

    Real men store their state in HDFS

---
+++ src/main/scala/com/datadudes/sf2hadoop/ImportState.scala
@@ -0,0 +1,43 @@
+package com.datadudes.sf2hadoop
+
+import java.io.IOException
+import java.net.URI
+import java.util.Calendar
+import javax.xml.bind.DatatypeConverter._
+
+import com.datadudes.sf2hadoop.SFImportCLIRunner.Config
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{Path, FileSystem}
+
+class ImportState(config: Config) {
+
+  val stateFile = if(config.stateFile.toString.startsWith("hdfs")) {
+      new Path(new URI(config.stateFile.getScheme, null, config.nnHostname, config.nnPort, config.stateFile.getPath, null, null))
+  } else new Path(config.stateFile)
+
+  val fs = stateFile.getFileSystem(new Configuration())
+
+  def readStates: Map[String, Calendar] = {
+    try {
+      val is = fs.open(stateFile)
+      scala.io.Source.fromInputStream(is).getLines().map { l =>
+        val entries = l.split(",")
+        entries(0) -> parseDateTime(entries(1))
+      }.toMap
+    } catch {
+      case e: IOException => Map[String, Calendar]()
+    }
+  }
+
+  def saveStates(states: Map[String, Calendar]) = {
+    val stateString = states.map(s => s._1 + "," + printDateTime(s._2)).mkString("\n")
+    val os = fs.create(stateFile, true)
+    os.writeBytes(stateString)
+    os.close()
+  }
+
+  def createDirs = {
+    fs.mkdirs(stateFile.getParent)
+  }
+
+}
--- src/main/scala/com/datadudes/sf2hadoop/SFImportCLIRunner.scala
+++ src/main/scala/com/datadudes/sf2hadoop/SFImportCLIRunner.scala
@@ -1,18 +1,19 @@
 package com.datadudes.sf2hadoop
 
 import java.io.File
+import java.net.URI
 import java.util.Calendar
 
 import com.datadudes.wsdl2avro.WSDL2Avro
 import AvroUtils._
-import ImportStates._
 import com.typesafe.scalalogging.LazyLogging
 
 object SFImportCLIRunner extends App with LazyLogging {
 
   case class Config(command: String = "", sfUsername: String = "", sfPassword: String = "",
                     nnHostname: String = "127.0.0.1", nnPort: Int = 8020, datasetBasePath: String = "",
-                    sfWSDL: File = new File("."), stateFile: File = new File("$HOME/.sf2hadoop/state"),
+                    sfWSDL: File = new File("."),
+                    stateFile: URI = new URI("file://" + System.getProperty("user.home") + "/.sf2hadoop/state"),
                     records: Seq[String] = Seq())
 
   val parser = new scopt.OptionParser[Config]("sf2hadoop") {
@@ -26,7 +27,7 @@ object SFImportCLIRunner extends App with LazyLogging {
     opt[Int]('o', "port") optional() action { (x, c) => c.copy(nnPort = x)} text "NodeName port"
     opt[String]('b', "basepath") required() action { (x, c) => c.copy(datasetBasePath = x)} text "Datasets basepath"
     opt[File]('w', "wsdl") required() valueName "<file>" action { (x, c) => c.copy(sfWSDL = x)} text "Path to Salesforce Enterprise WSDL"
-    opt[File]('s', "state") optional() valueName "<file>" action { (x, c) => c.copy(stateFile = x)} text "Path to state file to keep track of last updated timestamps"
+    opt[URI]('s', "state") optional() valueName "<URI>" action { (x, c) => c.copy(stateFile = x)} text "URI to state file to keep track of last updated timestamps"
     arg[String]("<record>...") unbounded() action { (x, c) => c.copy(records = c.records :+ x)} text "List of Salesforce record types to import"
     help("help") text "prints this usage text"
   }
@@ -44,20 +45,21 @@ object SFImportCLIRunner extends App with LazyLogging {
       val schemas = WSDL2Avro.convert(config.sfWSDL.getCanonicalPath, filterSFInternalFields)
       val connection = new SalesforceConnection(config.sfUsername, config.sfPassword)
       val importer = new SFImporter(schemas, config.nnHostname, config.nnPort, config.datasetBasePath, connection)
+      val state = new ImportState(config)
+      state.createDirs
 
       if(config.command == "init") {
-        createConfigDir
-        val existingStates = readStates(DEFAULT_STATE_FILE)
+        val existingStates = state.readStates
         val newStates = config.records.map { recordType =>
           val now = Calendar.getInstance()
           importer.initialImport(recordType)
           recordType -> now
         }.toMap
         val updatedStates = existingStates ++ newStates
-        saveStates(DEFAULT_STATE_FILE, updatedStates)
+        state.saveStates(updatedStates)
       }
       else {
-        val existingStates = readStates(DEFAULT_STATE_FILE)
+        val existingStates = state.readStates
         val newStates = config.records.flatMap { recordType =>
           existingStates.get(recordType) match {
             case Some(previous) => {
@@ -71,7 +73,7 @@ object SFImportCLIRunner extends App with LazyLogging {
             }
           }
         }.toMap
-        saveStates(DEFAULT_STATE_FILE, newStates)
+        state.saveStates(newStates)
       }
     }
   }
